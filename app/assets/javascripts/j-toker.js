8facfec0cc5bff73cf1e6ec1245a0130b50d1bf8
r00000000b <roberto.ching@gmail.com>
2016-10-26 11:04:18 +0800

    signup auth

--- app/assets/javascripts/j-toker.js
+++ app/assets/javascripts/j-toker.js
@@ -1,5 +1,48 @@
 $(document).ready(function(){
-  $('.collapsible').collapsible({
-    accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+  console.log('j-toker ready')
+
+  $.auth.configure({
+    apiUrl: 'http://localhost:3000'
   });
+
+  // var auth = {
+  //   signup: function() {
+  //     $.auth.emailSignUp({
+  //       email: 'test@test.com',
+  //       password: '*****',
+  //       password_confirmation: '*****'
+  //     }).then(function(user){
+  //       console.log('Welcome')
+  //     }).fail(function(user){
+  //       console.log('failure: ' + resp.errors.join(' '));
+  //     })
+  //   }
+  // }
+
+  // $.auth.emailSignUp({
+  //   email: 'test@test.com',
+  //   password: '*****',
+  //   password_confirmation: '*****',
+  //   config: 'altUser'
+  // });
+
+  var SignupClick = function(){
+    var params = {
+      email: $('#email').val(),
+      password: $('#password').val()
+    }
+
+    $.auth.emailSignUp(params).then(function(user){
+      alert('Welcome!');
+    }).fail(function(resp) {
+      alert('Authentication failure: ' + resp.errors.join(' '));
+    });
+  }
+
+  $('#signup-btn').click(function(e){
+    e.preventDefault();
+    SignupClick();
+  })
+
+
 });
