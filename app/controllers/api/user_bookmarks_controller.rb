25db621c06064d7f44671b58f9e2bb76c5a14805
DIVYA SAMTANI <Divyasamtani@gmail.com>
2016-10-31 10:03:47 +0800

    Can create bookmarks and see my bookmarks

--- app/controllers/api/user_bookmarks_controller.rb
+++ app/controllers/api/user_bookmarks_controller.rb
@@ -3,14 +3,13 @@
 class API::UserBookmarksController < ApplicationController
   before_action :authenticate_user!
   before_action :set_bookmarks
-  before_action :set_bookmark
+  before_action :set_bookmark, only: [:show, :update, :destroy]
 
   def index
-    render json: @bookmarks
   end
 
   def create
-    @bookmark = current_user.bookmarks.new(bookmark_params)
+    @bookmark = current_user.list_bookmarks.new(bookmark_params)
 
     if @bookmark.save
       render json: @bookmark
@@ -30,13 +29,13 @@ class API::UserBookmarksController < ApplicationController
     def set_bookmark
       @bookmark = current_user.bookmarks.find_by(id: params[:id])
       if @bookmark.nil?
-        render json: {message: "Cannot find '#{params[:id]}'"}
+        render json: {message: "Cannot find '#{params[:id]}'"}, status: 400
       end
     end
 
     def bookmark_params
-      params.require(:ListBookmark).permit(:user_id, :list_id)
-  end
+      params.require(:ListBookmark).permit(:list_id)
+    end
 end
 
 
